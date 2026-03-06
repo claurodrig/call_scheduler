26770e0c11cb1ff12e9c64c69c37a5f8320785b8
leandroirodriguez <leandro.rodriguez@gmail.com>
2026-03-06 18:17:37 -0500

    fix notification tap timing with delay

--- public/sw.js
+++ public/sw.js
@@ -17,15 +17,26 @@ self.addEventListener("notificationclick", event => {
   const data = event.notification.data || {};
   const action = data.action || "home";
 
-  // Broadcast to all tabs via BroadcastChannel
-  const bc = new BroadcastChannel("notif_nav");
-  bc.postMessage({ type: "NOTIF_NAV", action });
-  bc.close();
-
   event.waitUntil(
     clients.matchAll({ type: "window", includeUncontrolled: true }).then(clientList => {
-      if (clientList.length > 0) return clientList[0].focus();
-      return clients.openWindow("/?action=" + action);
+      const sendNav = () => {
+        const bc = new BroadcastChannel("notif_nav");
+        bc.postMessage({ type: "NOTIF_NAV", action });
+        bc.close();
+      };
+
+      if (clientList.length > 0) {
+        const client = clientList[0];
+        return client.focus().then(() => {
+          // Wait for app to be ready after focus
+          setTimeout(sendNav, 500);
+        });
+      } else {
+        // App wasn't open, opening fresh - wait longer for it to load
+        return clients.openWindow("/?action=" + action).then(() => {
+          setTimeout(sendNav, 1500);
+        });
+      }
     })
   );
 });
