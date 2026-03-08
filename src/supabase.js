636f3800d3af3c14b965552120bf0a0a904295e4
Claudia I. Rodriguez <claudiai.rodriguez@Claudias-MBP.lan>
2026-03-08 17:47:54 -0400

    fix supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://kqetdgoxqqibptoaarnc.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxZXRkZ294cXFpYnB0b2Fhcm5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MDcxMjgsImV4cCI6MjA4Nzk4MzEyOH0.G5eMbVhFLEhtowMZW-I0p6O-He4KsNlObjeQ1GaNnAo";
+const SUPABASE_URL = "https://tqqqwpigfptdjkmbieol.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxcXF3cGlnZnB0ZGprbWJpZW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5ODUzNzUsImV4cCI6MjA4ODU2MTM3NX0.gT1m_GM-CXfIa825n4V1YFvcMFwj13kjtc7tF86Voho";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
